4b145b91c94828e3297e2e5afd818cbefe8c250e
kinghatrung <nguyenred2003@gmail.com>
2025-11-06 23:50:57 +0700

    feat: created page sign in and sign up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import SignInPage from "~/pages/SignInPage";
+import SignUpPage from "~/pages/SignUpPage";
+import TaskApp from "~/pages/TaskApp";
+
 function App() {
-  return <>App</>;
+  return (
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<TaskApp />} />
+
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/TaskApp.tsx
@@ -0,0 +1,5 @@
+function TaskApp() {
+  return <>TaskApp</>;
+}
+
+export default TaskApp;
